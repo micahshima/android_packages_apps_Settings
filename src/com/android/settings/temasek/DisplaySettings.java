c78c0d11d365a2040c04204ad4429fa11f605c2d
Micah <micah.chickenfactory@gmail.com>
2015-04-24 19:49:41 -1000

    read from different display prefs file since LCD density moved

--- src/com/android/settings/temasek/DisplaySettings.java
+++ src/com/android/settings/temasek/DisplaySettings.java
@@ -57,7 +57,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        addPreferencesFromResource(R.xml.temasek_display_settings);
+        addPreferencesFromResource(R.xml.display);
 
         mContext = getActivity().getApplicationContext();
         int newDensityValue;
