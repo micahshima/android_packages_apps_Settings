cd4492f8c2ad1bb70829512017022d9e4f78b8fb
chickenfactory <micah.chickenfactory@gmail.com>
2015-05-16 15:33:30 -1000

    Revert "apps2sd: Only allow "move to ___" if the media is mounted [1/2]"
    This reverts commit 258fbbd02c8b40c0509709ff6c7b005408daa3a4.

--- src/com/android/settings/applications/ManageApplications.java
+++ src/com/android/settings/applications/ManageApplications.java
@@ -102,9 +102,7 @@ final class CanBeOnSdCardChecker {
     
     boolean check(ApplicationInfo info) {
         boolean canBe = false;
-        if (!PackageHelper.isExternalInstallPossible()) {
-            // Don't even bother checking the other cases, no media is available
-        } else if ((info.flags & ApplicationInfo.FLAG_EXTERNAL_STORAGE) != 0) {
+        if ((info.flags & ApplicationInfo.FLAG_EXTERNAL_STORAGE) != 0) {
             canBe = true;
         } else {
             if ((info.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
